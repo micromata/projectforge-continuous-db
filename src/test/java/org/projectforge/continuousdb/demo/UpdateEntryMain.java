4d1f1899458c0d338c3a062114f79e2cd04ac1b6
Kai Reinhard <K.Reinhard@micromata.de>
2013-05-10 17:12:56 +0200

    Initial revision.

---
+++ src/test/java/org/projectforge/continuousdb/demo/UpdateEntryMain.java
@@ -0,0 +1,170 @@
+/////////////////////////////////////////////////////////////////////////////
+//
+// Project ProjectForge Community Edition
+//         www.projectforge.org
+//
+// Copyright (C) 2001-2013 Kai Reinhard (k.reinhard@micromata.de)
+//
+// ProjectForge is dual-licensed.
+//
+// This community edition is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as published
+// by the Free Software Foundation; version 3 of the License.
+//
+// This community edition is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+// Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, see http://www.gnu.org/licenses/.
+//
+/////////////////////////////////////////////////////////////////////////////
+
+package org.projectforge.continuousdb.demo;
+
+import org.apache.commons.dbcp.BasicDataSource;
+import org.projectforge.common.DatabaseDialect;
+import org.projectforge.continuousdb.DatabaseUpdateDao;
+import org.projectforge.continuousdb.SchemaGenerator;
+import org.projectforge.continuousdb.Table;
+import org.projectforge.continuousdb.UpdateEntry;
+import org.projectforge.continuousdb.UpdateEntryImpl;
+import org.projectforge.continuousdb.UpdatePreCheckStatus;
+import org.projectforge.continuousdb.UpdateRunningStatus;
+import org.projectforge.continuousdb.UpdaterConfiguration;
+import org.projectforge.continuousdb.demo.entities.AccessEntryDO;
+import org.projectforge.continuousdb.demo.entities.Address1DO;
+import org.projectforge.continuousdb.demo.entities.Address2DO;
+import org.projectforge.continuousdb.demo.entities.GroupDO;
+import org.projectforge.continuousdb.demo.entities.GroupTaskAccessDO;
+import org.projectforge.continuousdb.demo.entities.TaskDO;
+import org.projectforge.continuousdb.demo.entities.UserDO;
+import org.projectforge.continuousdb.jdbc.DatabaseExecutorImpl;
+
+/**
+ * 
+ * @author Kai Reinhard (k.reinhard@micromata.de)
+ * 
+ */
+public class UpdateEntryMain
+{
+  public static void main(String[] args)
+  {
+    UpdateEntryMain main = null;
+    try {
+      main = new UpdateEntryMain();
+      // Create tables:
+      main.createInitialSchema();
+      // Create and update single table:
+      main.createAndUpdateAddressDO();
+    } finally {
+      if (main != null) {
+        main.shutdown();
+      }
+    }
+  }
+
+  private UpdaterConfiguration configuration;
+
+  private DatabaseUpdateDao databaseUpdateDao;
+
+  private UpdateEntryMain()
+  {
+    BasicDataSource dataSource = new BasicDataSource();
+    dataSource.setDriverClassName("org.hsqldb.jdbcDriver");
+    dataSource.setUsername("sa");
+    // dataSource.setPassword("password");
+    dataSource.setUrl("jdbc:hsqldb:testdatabase");
+
+    configuration = new UpdaterConfiguration();
+    configuration.setDialect(DatabaseDialect.HSQL);
+    final DatabaseExecutorImpl databaseExecutor = new DatabaseExecutorImpl();
+    databaseExecutor.setDataSource(dataSource);
+    configuration.setDatabaseExecutor(databaseExecutor);
+    databaseUpdateDao = new DatabaseUpdateDao(configuration);
+    // TableAttribute.register(new TableAttributeHookImpl());
+    configuration.setDatabaseUpdateDao(databaseUpdateDao);
+
+    // final SortedSet<UpdateEntry> updateEntries = new TreeSet<UpdateEntry>();
+    // updateEntries.addAll(DatabaseCoreUpdates.getUpdateEntries(this));
+    // getSystemUpdater().setUpdateEntries(updateEntries);
+  }
+
+  private void shutdown()
+  {
+    databaseUpdateDao.shutdownDatabase();
+  }
+
+  private void createAndUpdateAddressDO()
+  {
+    if (databaseUpdateDao.doesEntitiesExist(Address1DO.class) == false) {
+      // Initial creation of t_address because data-base table doesn't yet exist:
+      configuration.createSchemaGenerator().add(Address1DO.class).createSchema();
+    }
+    if (databaseUpdateDao.doesEntitiesExist(Address1DO.class) == false) {
+      throw new RuntimeException("What the hell? The table '" + Address1DO.class + "' wasn't created as expected!");
+    }
+    // Table t_address does now exist.
+    if (databaseUpdateDao.doesTableAttributesExist(Address2DO.class, "birthday", "address") == false) {
+      // One or both attributes don't yet exist, alter table to add the missing columns now:
+      databaseUpdateDao.addTableAttributes(Address2DO.class, "birthday", "address"); // Works also, if one of both attributes does already
+      // exist.
+    }
+    if (databaseUpdateDao.doesTableAttributesExist(Address2DO.class, "birthday", "address") == false) {
+      throw new RuntimeException("What the hell? The missing columns 'birthday' and 'address' weren't created as expected!");
+    }
+  }
+
+  private void createInitialSchema()
+  {
+    UpdateEntry initialUpdateEntry = getInitialUpdateEntry();
+    if (initialUpdateEntry.runPreCheck() == UpdatePreCheckStatus.READY_FOR_UPDATE) {
+      initialUpdateEntry.runUpdate();
+    }
+  }
+  
+  private UpdateEntry getInitialUpdateEntry() {
+    final Class< ? >[] doClasses = new Class< ? >[] { //
+    // Please note, the order of the entities is the order of their creation!
+        UserDO.class, //
+        TaskDO.class, GroupDO.class, TaskDO.class, GroupTaskAccessDO.class, //
+        AccessEntryDO.class, //
+    };
+
+    @SuppressWarnings("serial")
+    UpdateEntryImpl entry = new UpdateEntryImpl("core", "2013-05-10", "Adds all core tables T_*.") {
+
+      @Override
+      public UpdatePreCheckStatus runPreCheck()
+      {
+        // Does the data-base tables already exist?
+        if (databaseUpdateDao.doesEntitiesExist(doClasses) == false) {
+          return UpdatePreCheckStatus.READY_FOR_UPDATE;
+        }
+        return UpdatePreCheckStatus.ALREADY_UPDATED;
+      }
+
+      @Override
+      public UpdateRunningStatus runUpdate()
+      {
+        if (databaseUpdateDao.doesExist(new Table(UserDO.class)) == false && configuration.getDialect() == DatabaseDialect.PostgreSQL) {
+          // User table doesn't exist, therefore schema should be empty. PostgreSQL needs sequence for primary keys:
+          databaseUpdateDao.createSequence("hibernate_sequence", true);
+        }
+        final SchemaGenerator schemaGenerator = configuration.createSchemaGenerator().add(doClasses);
+        // You may add also add table manually, e. g. of third party libraries or such which aren't yet supported:
+        // final Table propertyDeltaTable = schemaGenerator.getTable(PropertyDelta.class);
+        // propertyDeltaTable.addAttribute(new TableAttribute("clazz", TableAttributeType.VARCHAR, 31).setNullable(false));
+        // final Table historyEntryTable = schemaGenerator.getTable(HistoryEntry.class);
+        // final TableAttribute typeAttr = historyEntryTable.getAttributeByName("type");
+        // typeAttr.setType(TableAttributeType.INT);
+        schemaGenerator.createSchema();
+        databaseUpdateDao.createMissingIndices(); // Create missing indices.
+
+        return UpdateRunningStatus.DONE;
+      }
+    };
+    return entry;
+  }
+}
